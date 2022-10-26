a0826e659aa2a1877ac148cd85169af9b51c06ed
MikKut <misha.kutny1@gmail.com>
2022-10-27 02:28:30 +0300

    Initial commit

---
+++ Logger/ILogger.cs
@@ -0,0 +1,15 @@
+﻿namespace Logger
+{
+    /// <summary>
+    /// Logs information in console.
+    /// </summary>
+    internal interface ILogger
+    {
+        /// <summary>
+        /// Logs information considering result <see cref="IResult"> and logtype <see cref="LogType">.
+        /// </summary>
+        /// <param name="result">Describes <see cref="IResult">.</param>
+        /// <param name="logType">Describes <see cref="LogType">.</param>
+        void LogInfo(IResult result, LogType logType);
+    }
+}
---
+++ Logger/Logger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    /// <summary>
+    /// Console logger that has three log types.
+    /// </summary>
+    internal class Logger : ILogger
+    {
+        private static Logger? instance;
+        private static StringBuilder? allRecordsOfTheSession;
+
+        private Logger()
+        {
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="Logger"/> class.
+        /// Writes all in theLog.txt file class.
+        /// </summary>
+        ~Logger()
+        {
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                w.Write(allRecordsOfTheSession!.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Gets all records that was done during the session.
+        /// </summary>
+        /// <returns>All records of the session.</returns>
+        public static string GetAllRecordsOfTheSession() => allRecordsOfTheSession!.ToString();
+
+        /// <summary>
+        /// Get logger sample(singleton).
+        /// </summary>
+        /// <returns>Singleton logger sample.</returns>
+        public static Logger GetLogger()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+                allRecordsOfTheSession = new StringBuilder();
+            }
+
+            return instance;
+        }
+
+        /// <summary>
+        /// Print in console logger info.
+        /// </summary>
+        /// <param name="result">Result of method execution.</param>
+        /// <param name="logType">Enum that describes <see cref="LogType">.</param>.
+        public void LogInfo(IResult result, LogType logType)
+        {
+            var currentLine = $"{DateTime.Now}:{logType}:{result.ErrorMessage}\n";
+            Console.Write(currentLine);
+            allRecordsOfTheSession!.Append(currentLine);
+        }
+    }
+}
